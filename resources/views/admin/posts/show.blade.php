df6e06f2a9aae102d3513eaf7872b5af0976bf53
Andrea Esposito <andexpox@gmail.com>
2021-06-21 17:49:47 +0200

    show

---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <h1>{{ $post->title }}</h1>
+
+        <div>Slug: {{ $post->slug }} </div>
+
+        <p> {{$post->content}} </p>
+    </div>
+    
+@endsection
--- resources/views/guest/posts/index.blade.php
+++ resources/views/guest/posts/index.blade.php
@@ -4,6 +4,19 @@
 
     <div class="container">
         <h1>Pubblicità</h1>
+
+        <div class="row">
+            @foreach($posts as $post)
+                <div class="col-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $post->title }}</h5>
+                            <a href="{{ route('blog', ['slug' => $post->slug]) }}" class="btn btn-primary">Leggi il post</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
     
 @endsection
